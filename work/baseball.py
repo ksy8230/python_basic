30f50309b02680f74d27a238fadae0f9025f60b0
ksy <ksy8230@hanmail.net>
2021-11-04 12:18:55 +0900

    야구 게임 1차 코드 리뷰

--- work/baseball.py
+++ work/baseball.py
@@ -4,11 +4,23 @@
 import os
 import random
 import time
+import keyboard
+
+
+class Colors:
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    WHITE = '\033[37m'
+    RESET = '\033[0m'
 
 
 class Util:
 
-    def makeRandomNumbers(maxNumber):  # 1~9까지 랜덤 숫자 maxNumber 자리 생성 함수
+    UP = 72
+    DOWN = 80
+
+    def makeRandomNumbers(self, maxNumber):  # 1~9까지 랜덤 숫자 maxNumber 자리 생성 함수
         pick_nums = set()
         while len(pick_nums) < maxNumber:
             pick_nums.add(str(random.randint(1, 9)))
@@ -16,7 +28,7 @@ class Util:
         ran_num = ''.join(list(pick_nums))
         return ran_num
 
-    def checkInputNumbers(inputNumbers):  # 올바른 입력인지 판별 함수
+    def checkInputNumbers(self, inputNumbers):  # 올바른 입력인지 판별 함수
         print("\n")
         if (inputNumbers == ""):
             print("입력값이 없습니다. 다시 입력해 주세요!")
@@ -36,7 +48,7 @@ class Util:
         else:
             return 1
 
-    def yesOrNo(message):  # 파라미터 값을 메세지로 질문하는 함수
+    def yesOrNo(self, message):  # 파라미터 값을 메세지로 질문하는 함수
         while True:
             reply = str(input(message + ' (y/n): ')).lower().strip()
             if reply[0] == 'y':
@@ -44,14 +56,50 @@ class Util:
             if reply[0] == 'n':
                 return False
 
-    def clearDisplay():  # 화면 정리 함수
+    def clearDisplay(self):  # 화면 정리 함수
         os.system(['clear', 'cls'][os.name == 'nt'])
 
-    def printTime(startTime, endTime):  # 초를 시분초로 변환하는 함수
+    def printTime(self, startTime, endTime):  # 초를 시분초로 변환하는 함수
         seconds = endTime - startTime
         result = time.strftime(
             "\n 경과한 시간 : %H 시간 %M 분 %S 초", time.gmtime(seconds))
         print(result)
+
+    def selectOne(self, value=1):  # 메뉴 선택하는 함수
+        self.clearDisplay()
+        self.printMenus(value)
+        while 1:
+            if keyboard.is_pressed(self.UP):  # 방향키 위 키보드
+                value -= 1
+                if value < 1:
+                    value = 2
+                self.clearDisplay()
+                self.printMenus(value)
+                time.sleep(0.1)
+
+            if keyboard.is_pressed(self.DOWN):  # 방향키 아래 키보드
+                value += 1
+                if value > 2:
+                    value = 1
+                self.clearDisplay()
+                self.printMenus(value)
+                time.sleep(0.1)
+
+            if keyboard.is_pressed('enter'):
+                return value
+
+    def printMenus(self, selected):  # 메뉴 그리는 함수
+        if selected == 1:
+            print(Colors.GREEN + '게임 시작' + Colors.RESET)
+        else:
+            print(Colors.WHITE + '게임 시작' + Colors.RESET)
+        if selected == 2:
+            print(Colors.RED + '게임 종료' + Colors.RESET)
+        else:
+            print(Colors.WHITE + '게임 종료' + Colors.RESET)
+
+
+util = Util()
 
 
 class BaseBallGame:
@@ -61,28 +109,22 @@ class BaseBallGame:
     ball_count = 0  # 볼 횟수
     out_count = 0  # 아웃 횟수
     records = []  # 게임 기록을 담은 배열
-    STATE_LIST = [{'title': '게임 시작', 'value': 1},
-                  {'title': '게임 종료', 'value': 0}]
-
     MAX_NUM_COUNT = 4  # 최대 숫자 갯수
-    numList = Util.makeRandomNumbers(MAX_NUM_COUNT)  # 컴퓨터 임의의 수 4자리
+    numList = util.makeRandomNumbers(MAX_NUM_COUNT)  # 컴퓨터 임의의 수 4자리
 
     def IntroGame(self):  # 게임 인트로 메소드
 
-        Util.clearDisplay()  # 화면 정리
-        for i in range(0, len(self.STATE_LIST)):
-            print(str(self.STATE_LIST[i]['value']) +
-                  ". "+self.STATE_LIST[i]['title'])
+        util.clearDisplay()  # 화면 정리
+        result = util.selectOne()  # 메뉴 선택
 
-        cmd = int(input("번호 입력 >> "))
-        if(cmd == self.STATE_LIST[0]['value']):
+        if(result == 1):
             self.playGame()
         else:
             self.exitGame()
 
     def continueGame(self, answer):  # 게임 계속 진행 여부 메소드
 
-        if (self.strike_count != self.MAX_NUM_COUNT and Util.yesOrNo("\n 재도전 하시겠습니까?") == 1):  # 스코어에 대한 기록 저장
+        if (self.strike_count != self.MAX_NUM_COUNT and util.yesOrNo("\n 재도전 하시겠습니까?") == 1):  # 스코어에 대한 기록 저장
             score_obj = {
                 "s": self.strike_count,
                 "b": self.ball_count,
@@ -98,7 +140,7 @@ class BaseBallGame:
 
         startTime = time.time()
         while (self.strike_count < 4):
-            Util.clearDisplay()  # 화면 정리
+            util.clearDisplay()  # 화면 정리
             # print(numList)  # 디버깅용 정답 (실제 게임 시엔 주석)
             print("\n<<<<< SCORE >>>>>")
             for i in self.records:
@@ -109,7 +151,7 @@ class BaseBallGame:
             print("==================\n")
             answer = str(input("4자리 수를 입력해주세요. >> "))
 
-            if (Util.checkInputNumbers(answer) == 1):
+            if (util.checkInputNumbers(answer) == 1):
                 self.strike_count = 0
                 self.ball_count = 0
                 self.out_count = 0
@@ -139,7 +181,7 @@ class BaseBallGame:
         if (self.strike_count == 4):  # 모두 스크라이크 시 경과 시간 출력
             endTime = time.time()
             print("정답!\n")
-            Util.printTime(startTime, endTime)
+            util.printTime(startTime, endTime)
             input("\n 종료 하려면 아무 키나 누르십시오...")
 
         self.exitGame()
